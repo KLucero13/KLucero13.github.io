c1fc6f113f4253f1e3ed7e194d0ff94eecf29935
KLucero13 <KLucero13@users.noreply.github.com>
2015-03-14 18:05:19 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
-
-  for(i in card_array){
-    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
+  var memory_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
+  var memory_tile_ids =[];
+  var tiles_flipped =0;
+Array.protype.memory_tile_shuffle =function(){
+  var i =this.length,j,temp;
+  while(--i>0){
+    j= Math.floor(Math.random()*(i+1));
+    temp=this[j];
+    this[j]=this[i];
+    this[i]=temp;
   }
-});
+}
+function newBoard(){
+  tiles_flipped=0;
+  var output='';
+  memory_array.memory_tile_shuffle();
+  for(var i=0;i<memory_array.length;i++){
+    otuput+='<div id= "tile'+i+' " onclick="memoryFlipTile(this,\'' +memory_array[i]+'\')"></div>';
+  }
+  document.getElementById('memory_board').innerHTML=output;
+}
